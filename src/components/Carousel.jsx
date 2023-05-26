c61c613cb08db8ca368e345a4e24c1ab776927d8
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-05-26 11:54:58 +0200

    fin css component header

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,41 +1,36 @@
-import "../style/carousel.css";
-import React, { useState, useEffect, Fragment } from "react";
+import '../style/carousel.css';
+import React, { useState} from "react";
 
-const Carousel = ({ locationPictures }) => {
-    const [index, setIndex] = useState(0);
-    const imgPrev = () => {
-        setIndex(
-            index === 0 ? locationPictures?.pictures?.length - 1 : index - 1
-        );
-    };
+
+const Carousel = ({ locationPictures}) => {
+  const [index, setIndex] = useState(0);
+  const imgPrev = () => {
+    setIndex(index=== 0 ? locationPictures?.length-1 : index-1);
+    }
 
     const imgNext = () => {
-        setIndex(
-            index === locationPictures?.pictures?.length - 1 ? 0 : index + 1
-        );
-    };
+      setIndex(index=== locationPictures?.length-1 ? 0 : index+1);
+      }
+  
     return (
-        <div className="container">
-            {locationPictures && locationPictures?.length > 0 && (
-                <img
-                    src={locationPictures[index]}
-                    alt={"location Pictures in detail"}
-                />
-            )}
-            {locationPictures?.length > 1 ? (
+      <div className="container">
+        { locationPictures &&
+          locationPictures?.length>0 
+          && <img className='img-carousel' src={locationPictures[index]}  alt={"location Pictures in detail" }/>
+          }
+        {locationPictures?.length > 1 ? (
                 <div className="arrow">
-                    <i
-                        onClick={imgPrev}
+                    <i onClick={imgPrev}
                         className="fa-solid fa-angle-left arrow-left"
                     ></i>
-                    <i
-                        onClick={imgNext}
+                    <i onClick={imgNext}
                         className="fa-solid fa-angle-right arrow-right"
                     ></i>
                 </div>
-            ) : null}
-        </div>
-    );
-};
+            ) : null}        
+      </div>
+    )
+  }
+
 
 export default Carousel;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import logo from "../assets/logo.webp";
 import "../style/Header.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function Header() {
     return (
         <div className="header">
-            <img src={logo} className="logo" alt="logo de kasa" />
+            <Link to="/"><img src={logo} className="logo" alt="logo de kasa"/></Link>
             <nav className="navbar">
                 <NavLink
                     className={({ isActive }) =>
