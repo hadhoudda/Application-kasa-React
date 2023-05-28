b581964b5a4197ba93e3716802deecb42f269f11
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-05-28 23:47:55 +0200

    update css desktop

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -2,7 +2,7 @@ import '../style/Host.css';
 
 function Host({locationHostPicture ,locationHostName}){
     return( 
-        <figure className='name-host'>
+        <figure className='host'>
           <figcaption> {locationHostName} </figcaption>
           <img className="img-host" src={locationHostPicture} alt='location' />
         </figure>
