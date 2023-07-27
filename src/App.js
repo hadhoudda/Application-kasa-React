1d28afd6fc41d267ebe14aaefc7e4c3b078bfc53
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-07-28 01:28:40 +0200

    deploye

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/details/:id" element={<Details />} />
+                <Route exact path="/Application-kasa-React" element={<Home />} />
+                <Route path="/Application-kasa-React/about" element={<About />} />
+                <Route path="/Application-kasa-React/details/:id" element={<Details />} />
                 <Route path="/*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
